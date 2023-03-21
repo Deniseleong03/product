c6340937fd98b8f50b51ae1b79c9ca4fbee8361a
DENISE LEONG <Your EMAILhauyee213024@dasein.edu.my>
2023-03-21 09:37:36 +0800

    changed nav bar

--- contactus.php
+++ contactus.php
@@ -7,28 +7,30 @@
   </head>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="http://localhost/project/home.php">Project</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="http://localhost/project/home.php">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="http://localhost/project/product_create.php">Create Product</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="http://localhost/project/createcustomers.php">Create Customer</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="http://localhost/project/contactus.php">Contact Us</a>
-      </li>
-    </ul>
-  </div>
-</nav>  
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark p-3">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    
+      <div class=" collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav ms-auto ">
+          <li class="nav-item">
+            <a class="nav-link mx-2 active" aria-current="page" href="http://localhost/project/home.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link mx-2" href="http://localhost/project/product_create.php#">Create Product</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link mx-2" href="http://localhost/project/createcustomers.php">Create Customers</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link mx-2" href="http://localhost/project/contactus.php">Contact Us</a>
+        </ul>
+      </div>
+    </div>
+    </nav>
 <body>
 
 <h1>This is a Contact page</h1>

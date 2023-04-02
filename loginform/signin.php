a506dc3f41d3f62440b337977ae5d313a04869b8
DENISE LEONG <Your EMAILhauyee213024@dasein.edu.my>
2023-04-02 21:52:18 +0800

    change title

--- loginform/signin.php
+++ loginform/signin.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
   <head>
-  	<title>Login 05</title>
+  	<title>Login</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -16,10 +16,55 @@
 
  <!-- container -->
     <div class="container">
-	<!-- PHP read one record will be here -->
-        <?php
-	
-	?>
+
+	<!-- PHP signin will be here -->
+       <?php
+	   session_start(); // Start the session
+	   
+	   // Check if the form has been submitted
+	   if ($_POST) {
+		   // include database connection
+	   	include 'config/database.php';
+		   // Get the form data
+	   	$username = htmlspecialchars(strip_tags($_POST['username']));
+		   $pass = htmlspecialchars(strip_tags($_POST['pass']));
+
+		   // Check if both fields are filled
+	   	if (empty($username)) {
+            $error_message = "Username is required";
+          }
+		   if (empty($pass)) {
+			   $error_message = "Password is required";
+		   }else {
+			   // Execute the query to retrieve the user's information
+	   		$query = "SELECT * FROM customers WHERE (username='$username' OR email='$username') AND status='active'";
+			   $result = mysqli_query($conn, $query);
+
+			   // Check if the query returned exactly one row
+	   		if (mysqli_num_rows($result) == 1) {
+				   $user = mysqli_fetch_assoc($result);
+
+				   // Verify the password hash
+	   			if (password_verify($pass, $user['pass'])) {
+					
+					   // Password is correct, set the session variable and redirect to home page
+	   				$_SESSION['user_id'] = $user['id'];
+					   header('Location: home.php');
+					   exit();
+				   } else {
+					   // Password is incorrect
+	   				$error_message = 'Incorrect password.';
+				   }
+			   } else {
+				   // Username/email not found or account is inactive
+	   			$error_message = 'Incorrect username/email or account is inactive.';
+			   }
+			   
+		   }
+	   }
+
+	   ?>
+
 
 	<section class="ftco-section">
 		<div class="container">
@@ -36,12 +81,7 @@
 			      		<div class="w-100">
 			      			<h3 class="mb-4">Sign In</h3>
 			      		</div>
-								<div class="w-100">
-									<p class="social-media d-flex justify-content-end">
-										<a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-facebook"></span></a>
-										<a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-twitter"></span></a>
-									</p>
-								</div>
+								
 			      	</div>
 							<form action="#" class="signin-form">
 			      		<div class="form-group mt-3">
@@ -75,6 +115,13 @@
 			</div>
 		</div>
 	</section>
+
+	<!-- Display an error message if the login failed -->
+<?php if (isset($error_message)) { ?>
+		<p>
+			<?php echo $error_message; ?>
+		</p>
+	<?php } ?>
 
 
 
